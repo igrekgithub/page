6e8fcc86574602bfa32f492d99832c05f1e10459
igrekgithub <ivshome.mybox@gmail.com>
2023-07-20 23:52:49 +0500

    first

--- is.js
+++ is.js
@@ -1,13 +1,13 @@
-const h1 =document.querySelector('h1')
+const h1 = document.querySelector('h1')
 const newEl = document.createElement('div')
-newEl.innerHTML = `<span  class="hi">Всем Привет!!!!</span>`
+newEl.innerHTML = `<span  class="hi">Всем Привет!</span>`
 console.log(newEl)
 h1.after(newEl)
 const newElem = document.createElement('ul')
 newElem.innerHTML =
-`<li>Пункт 1</li>
+	`<li>Пункт 1</li>
  <li>Пункт 2</li>
  <li>Пункт 3</li>
  <li>Пункт 4</li>`
- newElem.classList.add('roster')
+newElem.classList.add('roster')
 newEl.append(newElem)
