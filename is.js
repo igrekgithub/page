f15d8d2994460bd7827f3e06b9e40a88bcabde4e
igrekgithub <ivshome.mybox@gmail.com>
2023-07-21 18:24:00 +0500

    img

--- is.js
+++ is.js
@@ -5,9 +5,9 @@ console.log(newEl)
 h1.after(newEl)
 const newElem = document.createElement('ul')
 newElem.innerHTML =
-	`<li>Пункт 1</li>
- <li>Пункт 2</li>
- <li>Пункт 3</li>
- <li>Пункт 4</li>`
+	 `<li>Пункт 1</li>
+ 		<li>Пункт 2</li>
+ 		<li>Пункт 3</li>
+ 		<li>Пункт 4</li>`
 newElem.classList.add('roster')
 newEl.append(newElem)
